15c35810a159970005336904246c588b5c9d381d
1000007201 <nishant.sharma5504@gmail.com>
2022-02-16 16:44:55 +0530

    [Nishant] Add:Prime Factors Done

---
+++ Basic-Core-Programs/PrimeFactors.py
@@ -0,0 +1,18 @@
+
+def primeFactors(number):
+    while number % 2 == 0:
+        print(2,)
+        number = number / 2
+
+    for i in range(3, int(number**(1/2)) + 1, 2):
+
+        while number % i == 0:
+            print(i,)
+            number = number / i
+
+    if number > 2:
+        print(number)
+
+
+number = int(input("Enter number:"))
+primeFactors(number)
