14615cea78ba2b8fa80204b988853bd78019afaf
1000007201 <nishant.sharma5504@gmail.com>
2022-02-16 16:28:29 +0530

    [Nishant] Add:Power of two done

--- Basic-Core-Programs/LeapYear.py
+++ Basic-Core-Programs/LeapYear.py
@@ -1,5 +1,5 @@
 year = int(input("Enter Year you have to evaluate:"))
-if year % 4 == 0:
+if 0 == year % 4:
     print(f"{year} is a Leap Year")
 else:
     print(f"{year} is not a Leap Year")
---
+++ Basic-Core-Programs/Power.py
@@ -0,0 +1,3 @@
+number = int(input("Enter maximum number of power:"))
+for i in range(1, number+1):
+    print(f"2 ^ {i} = {2**i}")
