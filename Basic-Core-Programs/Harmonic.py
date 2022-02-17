837e2f6b23dff993b4107d383db29fa3e7b66c55
1000007201 <nishant.sharma5504@gmail.com>
2022-02-17 15:44:40 +0530

    [Nishant] Refactor:Harmonic program got updated

--- Basic-Core-Programs/Harmonic.py
+++ Basic-Core-Programs/Harmonic.py
@@ -1,5 +1,13 @@
-number = int(input("Enter value of number:"))
-harmonic = 0
-for i in range(1, number+1):
-    harmonic += 1/i
-print(harmonic)
+def Harmonic():
+    '''
+    Taking Input of number from user.
+    :return: Harmonic value of given number
+    '''
+    number = int(input("Enter value of number:"))
+    harmonic = 0
+    for i in range(1, number+1):
+        harmonic += 1/i
+    return harmonic
+
+print(Harmonic())
+print(Harmonic.__doc__)#Print Doc String of Function Harmonic.
